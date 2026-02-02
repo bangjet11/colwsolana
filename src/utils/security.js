67c4df0967b7a318f22465a0f68e663cdeb16fc4
bangjet11 <rokomaxwin@gmail.com>
2026-02-02 18:51:01 +0700

    Create security.js

---
+++ src/utils/security.js
@@ -0,0 +1,16 @@
+export function getDeviceInfo() {
+  const ua = navigator.userAgent;
+  if (/mobile/i.test(ua)) return "Mobile";
+  if (/tablet/i.test(ua)) return "Tablet";
+  return "Desktop";
+}
+
+export async function getIP() {
+  try {
+    const res = await fetch("https://api.ipify.org?format=json");
+    const data = await res.json();
+    return data.ip;
+  } catch {
+    return "Unknown IP";
+  }
+}
